277662490f4e31c5540538c9d95f102335b876af
Matthias Rustler <mrustler@gmx.de>
2020-01-18 04:52:47 +0100

    use "extend" to append a list

--- scripts/docutils/writers/html4css1/__init__.py
+++ scripts/docutils/writers/html4css1/__init__.py
@@ -229,7 +229,7 @@ class HTMLTranslator(nodes.NodeVisitor):
             self.html_prolog.append(self.xml_declaration)
         self.head_prefix.extend([self.doctype,
                                  self.head_prefix_template % (lcode, lcode)])
-        self.html_prolog.append([self.phpenc % settings.output_encoding, self.doctype])
+        self.html_prolog.extend([self.phpenc % settings.output_encoding, self.doctype])
         self.head = self.meta[:]
         stylesheet = utils.get_stylesheet_reference(settings)
         self.stylesheet = []
